34a5ef57e3dceecd4dc532a795646b7edf2d67b9
zhangzheyuk@163.com <zhangzheyuk@163.com>
2016-06-03 12:10:08 +0800

    flash lesson.

--- startX.py
+++ startX.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, session, redirect, url_for
+from flask import Flask, render_template, session, redirect, url_for, flash
 # from flask_bootstrap3 import Bootstrap
 from flask.ext.bootstrap import Bootstrap
 from datetime import datetime
@@ -12,12 +12,17 @@ app.config['SECRET_KEY'] = 'hard to x'
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    name = None
+    # name = None
     testform = NameForm()
     if testform.validate_on_submit():
+        old_name = session.get('name')
+        if old_name is not None and old_name != testform.name.data:
+            flash('Looks like you had changed your name!')
         session['name'] = testform.name.data
         return redirect(url_for('index'))
-    return render_template('index.html', current_time=datetime.utcnow(), form=testform, name=session.get('name'))
+    return render_template('index.html',
+                           current_time=datetime.utcnow(),
+                           form=testform, name=session.get('name'))
     # return render_template('index.html', form=testform)
 
 
